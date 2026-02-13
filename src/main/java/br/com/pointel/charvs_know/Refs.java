ba802eede7be82c729723cf51f0ec2242b953f82
Éverton M. Vieira <emuvi@outlook.com.br>
2026-02-13 11:32:37 -0300

    feat: refs

---
+++ src/main/java/br/com/pointel/charvs_know/Refs.java
@@ -0,0 +1,39 @@
+package br.com.pointel.charvs_know;
+
+import java.io.File;
+
+import org.apache.commons.io.FilenameUtils;
+import org.apache.tools.ant.types.selectors.FilenameSelector;
+
+import br.com.pointel.jarch.flow.FTP;
+import br.com.pointel.jarch.mage.WizBytes;
+
+public class Refs {
+
+    private static final FTP ftp = new FTP(
+        System.getenv("CHARVS_KNOW_REFS_HOST"), 
+        Integer.parseInt(System.getenv("CHARVS_KNOW_REFS_PORT")), 
+        System.getenv("CHARVS_KNOW_REFS_USER"), 
+        System.getenv("CHARVS_KNOW_REFS_PASS"));
+
+    public static void upload(File origin) throws Exception {
+        if (!ftp.isConnected()) {
+            ftp.connect();
+        }
+        var md5 = WizBytes.getMD5(origin);
+        var ext = FilenameUtils.getExtension(origin.getName());
+        var destiny = md5 + (ext.isEmpty() ? "" : "." + ext);
+        var folder = "public_html/refs/" + md5.substring(0, 3);
+        ftp.openWithMakeDir(folder);
+        ftp.upload(origin, destiny);
+    }
+
+    public static void download(String ref, File destiny) throws Exception {
+        if (!ftp.isConnected()) {
+            ftp.connect();
+        }
+        var url = "public_html/refs/" + ref.substring(0, 3) + "/" + ref;
+        ftp.download(url, destiny);
+    }
+
+}
