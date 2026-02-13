1afe8b78495a418ad083d06783e9ff43b1a42aad
Éverton M. Vieira <emuvi@outlook.com.br>
2026-02-13 11:32:11 -0300

    feat: steps

---
+++ src/main/java/br/com/pointel/charvs_know/Steps.java
@@ -0,0 +1,32 @@
+package br.com.pointel.charvs_know;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
+public enum Steps { 
+    
+    Identificar, Classificar, Organizar, Atomizar, Questionar, Explicar;
+
+    public String getCommand() throws Exception {
+        return Files.readString(list.get(this.ordinal()).toPath());
+    }
+
+    private static final List<File> list = new ArrayList<>();
+
+    static {
+        var folder = new File("steps");
+        if (folder.exists()) {
+            var files = folder.listFiles();
+            if (files != null) {
+                for (var file : files) {
+                    if (file.isFile()) {
+                        list.add(file);
+                    }
+                }
+                list.sort((a, b) -> a.getName().compareTo(b.getName()));
+            }
+        }
+    }
+}
