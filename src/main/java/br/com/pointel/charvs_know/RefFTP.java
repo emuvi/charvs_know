ffa99a4d12a687adf23ff620ff3e674c1e01a54b
Éverton M. Vieira <emuvi@outlook.com.br>
2026-02-15 10:19:05 -0300

    feat: ftp functions

--- src/main/java/br/com/pointel/charvs_know/RefFTP.java
+++ src/main/java/br/com/pointel/charvs_know/RefFTP.java
@@ -6,14 +6,22 @@ import org.apache.commons.io.FilenameUtils;
 
 import br.com.pointel.jarch.flow.FTP;
 import br.com.pointel.jarch.mage.WizBytes;
+import br.com.pointel.jarch.mage.WizEnv;
 
 public class RefFTP {
 
+
+    public static final String BASE_URI = WizEnv.get("CHARVS_KNOW_REFS_FTP_BASE_URI", "https://urvs.com.br/");
+    public static final String BASE_FOLDER = WizEnv.get("CHARVS_KNOW_REFS_FTP_BASE_FOLDER", "know/refs/");
+    public static final String BASE_PUBLIC = WizEnv.get("CHARVS_KNOW_REFS_FTP_BASE_PUBLIC", "public_html/");
+    public static final String BASE_URI_FOLDER = BASE_URI + BASE_FOLDER;
+
+
     private static final FTP ftp = new FTP(
-        System.getenv("CHARVS_KNOW_REFS_FTP_HOST"), 
-        Integer.parseInt(System.getenv("CHARVS_KNOW_REFS_FTP_PORT")), 
-        System.getenv("CHARVS_KNOW_REFS_FTP_USER"), 
-        System.getenv("CHARVS_KNOW_REFS_FTP_PASS"));
+        WizEnv.get("CHARVS_KNOW_REFS_FTP_HOST", "ftp.urvs.com.br"),
+        WizEnv.get("CHARVS_KNOW_REFS_FTP_PORT", 21),
+        WizEnv.get("CHARVS_KNOW_REFS_FTP_USER", "urvs1"),
+        WizEnv.get("CHARVS_KNOW_REFS_FTP_PASS", ""));
 
     public static void upload(File origin) throws Exception {
         if (!ftp.isConnected()) {
@@ -21,18 +29,37 @@ public class RefFTP {
         }
         var md5 = WizBytes.getMD5(origin);
         var ext = FilenameUtils.getExtension(origin.getName());
-        var destiny = md5 + (ext.isEmpty() ? "" : "." + ext);
-        var folder = "public_html/refs/" + md5.substring(0, 3);
-        ftp.openWithMakeDir(folder);
-        ftp.upload(origin, destiny);
+        var refWithExtension = md5 + (ext.isEmpty() ? "" : "." + ext);
+        ftp.openWithMakeDir(getBaseFTPFolder(refWithExtension));
+        ftp.upload(origin, refWithExtension);
+    }
+
+    public static void upload(File origin, String refWithExtension) throws Exception {
+        if (!ftp.isConnected()) {
+            ftp.connect();
+        }
+        ftp.openWithMakeDir(getBaseFTPFolder(refWithExtension));
+        ftp.upload(origin, refWithExtension);
     }
 
     public static void download(String refWithExtension, File destiny) throws Exception {
         if (!ftp.isConnected()) {
             ftp.connect();
         }
-        var url = "public_html/refs/" + refWithExtension.substring(0, 3) + "/" + refWithExtension;
-        ftp.download(url, destiny);
+        var remote = getBaseFTPFolder(refWithExtension) + "/" + refWithExtension;
+        ftp.download(remote, destiny);
+    }
+
+    public static String getBaseFTPFolder(String refWithExtension) {
+        return BASE_PUBLIC + BASE_FOLDER + refWithExtension.substring(0, 3);
+    }
+
+    public static String getBaseURIFolder(String refWithExtension) {
+        return BASE_URI + BASE_FOLDER + refWithExtension.substring(0, 3);
+    }
+
+    public static String getBaseURI(String refWithExtension) {
+        return getBaseURIFolder(refWithExtension) + "/" + refWithExtension;
     }
 
 }
